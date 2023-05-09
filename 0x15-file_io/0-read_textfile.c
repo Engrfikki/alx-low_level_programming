c4daf2c3626da975da3f19ea3d3fce3ed98e13ac
engrfikki <babatundeoyes@gmail.com>
2023-05-09 04:25:31 -0700

    correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,11 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t fik, xtun;
-	ssize_t fd;
 	char *buf;
-
-	if (!filename)
-		return (0);
 
 	fd = open(filename, O_RDONLY);
 
@@ -24,9 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
+	
 	fik = read(fd, buf, letters);
 	xtun = write(STDOUT_FILENO, buf, fik);
 
