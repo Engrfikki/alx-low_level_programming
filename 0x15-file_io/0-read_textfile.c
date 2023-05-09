67759a3b3e581619244700a595aa8aa71a8403c4
engrfikki <babatundeoyes@gmail.com>
2023-05-08 17:25:22 -0700

    correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,33 +5,33 @@
  * @filename: filename.
  * @letters: number of letters printed.
  *
- * Return: if filename is NULL,if write fails, file cannot be opened 
- * return 0.
+ * Return: number of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
+
 {
-	int st;
-	ssize_t mka, hal;
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
-	st = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (st == -1)
+	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
-	mka = read(st, buf, letters);
-	hal = write(STDOUT_FILENO, buf, mka);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	close(st);
+	close(fd);
 
 	free(buf);
 
-	return (hal);
+	return (nwr);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,23 +2,23 @@
 
 /**
  * create_file - creates a file
- * @filename: file to create.
+ * @filename: filename.
  * @text_content: content writed in the file.
- *
- * Return: 1 if it success. -1 on failure.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
+
 {
-	int st;
+	int fd;
 	int nletters;
-	int mka;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	st = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (st == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
@@ -27,12 +27,12 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	mka = write(st, text_content, nletters);
+	rwr = write(fd, text_content, nletters);
 
-	if (mka == -1)
+	if (rwr == -1)
 		return (-1);
 
-	close(st);
+	close(fd);
 
 	return (1);
 }
